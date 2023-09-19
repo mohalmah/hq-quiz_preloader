d4e220e555b5586b56853a98931daa2d3817db48
Jasminder Pal Singh <ghulianisikh@gmail.com>
2023-09-20 01:52:38 +0530

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,50 +1,21 @@
-// Create the overlay and SVG elements
-const overlay = document.createElement('div');
-overlay.id = 'loader';
-overlay.style.cssText = `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(255, 255, 255, 0.7);
-    z-index: 999;
-`;
+function isCalendlyEvent(e) {
+            return e.origin === "https://calendly.com" && e.data.event && e.data.event.indexOf("calendly.") === 0;
+        };
 
-const svgImage = document.createElement('img');
-svgImage.id = 'svgImage';
-svgImage.src = 'https://cdn.jsdelivr.net/gh/jasminder/flutter_preloader/6.svg';
-svgImage.style.cssText = `
-    max-width: 100%;
-    max-height: 100%;
-    display: none;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-`;
-svgImage.style.display = 'none';
+        window.addEventListener("message", function(e) {
+            if (isCalendlyEvent(e)) {
+                /* Example to get the name of the event */
+                console.log("Event name:", e.data.event);
 
-// Append the elements to the body
-document.body.appendChild(overlay);
-document.body.appendChild(svgImage);
-
-// Function to hide the overlay and display the SVG
-function hideOverlay() {
-    overlay.style.display = 'none';
-    svgImage.style.display = 'block';
-}
-
-// Add an event listener to hide the overlay when all external JS files are loaded
-document.addEventListener('DOMContentLoaded', () => {
-    // Replace the following lines with the actual code that loads your external JS files
-    // For demonstration purposes, we'll use a setTimeout to simulate loading external JS files.
-    setTimeout(hideOverlay, 100); // Replace with your actual loading code.
-});
-
-// Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
-// we'll still hide the overlay when the window's load event is triggered.
-window.addEventListener('load', hideOverlay);
+                /* Example to get the payload of the event */
+                console.log("Event details:", e.data.payload);
+                if (e.data.event == "calendly.event_scheduled") {
+                    var event_url = e.data.payload.event.uri;
+                    console.log(e.data.payload.event.uri);
+                    console.log("All good");
+                    const parts = event_url.split('/');
+                    const lastId = parts[parts.length - 1];
+                    window.top.location.href = "https://thetokenspace.com/sessions?event_id="+lastId;
+                }
+            }
+        });
