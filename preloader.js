573708ad91ff1914e8ccf06a57891ab848b69474
Mohammed Mahfadi (Kali) <44398858+mohalmah@users.noreply.github.com>
2023-12-08 12:50:35 +0800

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -18,11 +18,11 @@ const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
 svgImage.src = 'https://cdn.jsdelivr.net/gh/mohalmah/hq-quiz_preloader/hq_loader.svg';
 svgImage.style.cssText = `
-    width: 100px;
-    height: 100px;
+    width: 200px;
+    height: 200px;
     display: block;
     position: absolute;
-    top: 10%;
+    top: 30%;
     left: 50%;
     transform: translate(-50%, -50%);
 `;
@@ -40,52 +40,52 @@ textContainer.style.cssText = `
     transform: translate(-50%, -50%);
     text-align: center;
     width: 100%;
-    display: none;
 `;
 
-// Append the text container to the body
 document.body.appendChild(textContainer);
 
-// Add the text lines to the container
+// Updated text lines content with FontAwesome icons
 const textLinesContent = [
-    'Checking available seats',
-    'seats available', // Placeholder for random number
-    '1 seat reserved for you',
-    'Please complete quiz to check if you’re a fit',
-    'Quiz starting now…'
+    { text: 'Checking available seats', icon: 'fas fa-chair' },
+    { text: 'seats available', icon: 'fas fa-users' }, // Placeholder for random number
+    { text: '1 seat reserved for you', icon: 'fas fa-user-check' },
+    { text: 'Please complete quiz to check if you’re a fit', icon: 'fas fa-puzzle-piece' },
+    { text: 'Quiz starting now…', icon: 'fas fa-hourglass-start' }
 ];
 
-textLinesContent.forEach((text, index) => {
-    const line = document.createElement('p');
-    line.style.cssText = `
-        margin: 0;
-        padding: 0;
-        font-size: 1.2em;
-        color: #333; // Change the color as needed
-        opacity: 0;
-        transition: opacity 2s ease-in-out;
-    `;
-    line.innerText = text;
+textLinesContent.forEach((item, index) => {
+    const line = document.createElement('div');
+    line.className = 'text-box';
     line.id = `textLine${index}`;
+
+    // Create and append the FontAwesome icon
+    const icon = document.createElement('i');
+    icon.className = item.icon;
+    icon.style.cssText = 'margin-right: 10px;'; // Styling for the icon
+
+    // Append icon and text to the line
+    line.appendChild(icon);
+    line.appendChild(document.createTextNode(item.text));
+
     textContainer.appendChild(line);
 });
 
 // Function to show and animate the text lines
 function animateTextLines() {
-    let delay = 500; // Initial delay before the first line appears
+    let delay = 500; // Initial delay
 
     textLinesContent.forEach((_, index) => {
         setTimeout(() => {
             const line = document.getElementById(`textLine${index}`);
             line.style.opacity = '1';
-            // Update the 'seats available' line with a random number
+            line.style.transform = 'translateY(0)';
             if (index === 1) {
-                const randomSeats = Math.floor(Math.random() * 20);
+                const randomSeats = Math.floor(Math.random() * 20) + 2; // Ensure at least 1 seat
                 line.innerText = `${randomSeats} seats available`;
             }
         }, delay);
 
-        delay += 1000; // Increment delay for the next line
+        delay += 700; // Increment delay for the next line
     });
 }
 
@@ -100,3 +100,28 @@ function hideOverlay() {
 // Add event listeners for DOMContentLoaded and load events
 document.addEventListener('DOMContentLoaded', hideOverlay);
 window.addEventListener('load', hideOverlay);
+
+// CSS for text boxes
+const style = document.createElement('style');
+style.textContent = `
+.text-box {
+    background-color: #f2f2f2; /* Light grey background */
+    border: 1px solid #dcdcdc; /* Slight border for definition */
+    border-radius: 10px; /* Rounded corners */
+    padding: 10px 20px; /* Padding inside the box */
+    margin: 10px auto; /* Margin for spacing between boxes, centered horizontally */
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Subtle shadow for depth */
+    transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out; /* Smooth transition for animation */
+    opacity: 0; /* Start with box invisible */
+    transform: translateY(20px); /* Start slightly lower */
+    width: 80%; /* Responsive width */
+    max-width: 400px; /* Maximum width */
+    text-align: center; /* Center the text */
+    font-size: 1.2em; /* Larger text */
+    display: flex; /* Flex display to align icon and text */
+    align-items: center; /* Center items vertically */
+    justify-content: center; /* Center items horizontally */
+}
+`;
+
+document.head.appendChild(style);
